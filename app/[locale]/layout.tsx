b708925dbdce1cb54eddee96ea9aaa9ba3054062
Vlad <hotsru12@gmail.com>
2026-02-10 18:16:46 +0300

    Add Google Tag Manager integration to layout component for improved tracking

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -32,8 +32,13 @@ export default async function LocaleLayout({
   return (
     <html lang={locale} suppressHydrationWarning>
       <head>
-        {/* Block search engine indexing - site is in development */}
-        <meta name="robots" content="noindex, nofollow" />
+        {/* Google Tag Manager */}
+        <script dangerouslySetInnerHTML={{__html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-KWVBDZ29');`}} />
+        {/* End Google Tag Manager */}
         
         {/* Load Manrope from Google Fonts for all locales */}
         <link rel="preconnect" href="https://fonts.googleapis.com" />
@@ -41,6 +46,10 @@ export default async function LocaleLayout({
         <link href="https://fonts.googleapis.com/css2?family=Manrope:wght@400;500;600;700&display=swap" rel="stylesheet" />
       </head>
       <body suppressHydrationWarning>
+        {/* Google Tag Manager (noscript) */}
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KWVBDZ29"
+height="0" width="0" style={{display:'none',visibility:'hidden'}}></iframe></noscript>
+        {/* End Google Tag Manager (noscript) */}
         <NextIntlClientProvider messages={messages}>
           <SiteBanner />
           <Header locale={locale as Locale} />
