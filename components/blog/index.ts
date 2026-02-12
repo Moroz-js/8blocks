6c92dbdd1bde22a0ad1b8fc16cb80508094e856a
Vlad <hotsru12@gmail.com>
2026-02-12 13:29:28 +0300

    Remove TagList export from blog index to streamline component exports and improve code clarity.

--- components/blog/index.ts
+++ components/blog/index.ts
@@ -2,7 +2,6 @@ export { default as BlogCard } from '../cards/BlogCard';
 export { BlogGrid } from './BlogGrid';
 export { CategoryFilter } from './CategoryFilter';
 export { default as CategoryDropdown } from './CategoryDropdown';
-export { TagList } from './TagList';
 export { ShareButtons } from './ShareButtons';
 export { TableOfContents } from './TableOfContents';
 export { RelatedArticles } from './RelatedArticles';
